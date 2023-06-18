9589d3acd8d070c3deb04776e75a15dddf2a8eae
Joseph Clover <clover@Josephs-MacBook-Pro.local>
2023-06-18 17:03:35 +0100

    Quick Format

--- src/app.ts
+++ src/app.ts
@@ -10,36 +10,36 @@ const buildServer = (opts = {}) => {
     const data = await req.file();
 
     const parseTask: Promise<number> = new Promise((resolve, reject) => {
-        const fs = new FrameService();
+      const fs = new FrameService();
 
-        if (!data) {
-          return reject("Invalid File");
+      if (!data) {
+        return reject("Invalid File");
+      }
+
+      data.file.on("error", (err) => {
+        reject(err.message);
+      });
+
+      data.file.on("data", (chunk: Buffer) => {
+        fs.readChunk(chunk);
+      });
+
+      data.file.on("end", () => {
+        if (fs.frameCount > 0) {
+          resolve(fs.frameCount);
+        } else {
+          reject("Unrecognized File Format");
         }
-
-        data.file.on('error', (err) => {
-            reject(err.message);
-        });
-        
-        data.file.on('data', (chunk: Buffer) => {
-            fs.readChunk(chunk);
-        });
-    
-        data.file.on('end', () => {
-            if (fs.frameCount > 0) {
-                resolve(fs.frameCount);
-            } else {
-                reject("Unrecognized File Format");
-            }
-        });    
+      });
     });
 
     try {
-        const frameCount = await parseTask;
-        res.status(200).send({
-            frameCount
-        });
+      const frameCount = await parseTask;
+      res.status(200).send({
+        frameCount,
+      });
     } catch (e) {
-        res.status(500).send(e);
+      res.status(500).send(e);
     }
   });
 
--- src/services/frame-service.spec.ts
+++ src/services/frame-service.spec.ts
@@ -1,50 +1,44 @@
-import FrameService from './frame-service';
-import tap from 'tap';
+import FrameService from "./frame-service";
+import tap from "tap";
 
-tap.test('correctly identifies the number of frames within a buffer', (t) => {
-    const fs = new FrameService();
-    const buf = new Uint8Array([
-        0xF1, 0xAA, 0x33, 0x93, 0xFF, 0xFB, 0x1A, 0x22, 0x44, 0x45, 0xFF, 0xFB
-    ]);
+tap.test("correctly identifies the number of frames within a buffer", (t) => {
+  const fs = new FrameService();
+  const buf = new Uint8Array([
+    0xf1, 0xaa, 0x33, 0x93, 0xff, 0xfb, 0x1a, 0x22, 0x44, 0x45, 0xff, 0xfb,
+  ]);
 
-    fs.readChunk(Buffer.from(buf));
-    
-    t.equal(fs.frameCount, 2, 'Should contain 2 frames.');
-    t.end();
+  fs.readChunk(Buffer.from(buf));
+
+  t.equal(fs.frameCount, 2, "Should contain 2 frames.");
+  t.end();
 });
 
-tap.test('correctly identifies the number of frames across buffers', (t) => {
-    const fs = new FrameService();
-    const buf1 = new Uint8Array([
-        0x00, 0xFF,
-    ]);
+tap.test("correctly identifies the number of frames across buffers", (t) => {
+  const fs = new FrameService();
+  const buf1 = new Uint8Array([0x00, 0xff]);
 
-    const buf2 = new Uint8Array([
-        0xF0, 0x00
-    ])
+  const buf2 = new Uint8Array([0xf0, 0x00]);
 
-    fs.readChunk(Buffer.from(buf1));
-    t.equal(fs.frameCount, 0, 'should have no frames');
-    fs.readChunk(Buffer.from(buf2));
-    t.equal(fs.frameCount, 1, 'should have 1 frame');
+  fs.readChunk(Buffer.from(buf1));
+  t.equal(fs.frameCount, 0, "should have no frames");
+  fs.readChunk(Buffer.from(buf2));
+  t.equal(fs.frameCount, 1, "should have 1 frame");
 
-    t.end();
-})
+  t.end();
+});
 
-tap.test('clearing the service', (t) => {
-    const fs = new FrameService();
-    const buf = new Uint8Array([
-        0xFF, 0xFB, 0x00, 0xFF
-    ]);
+tap.test("clearing the service", (t) => {
+  const fs = new FrameService();
+  const buf = new Uint8Array([0xff, 0xfb, 0x00, 0xff]);
 
-    fs.readChunk(Buffer.from(buf));
+  fs.readChunk(Buffer.from(buf));
 
-    t.equal(fs["_prevByte255"], true, 'should have prevByte before clear');
-    t.equal(fs.frameCount, 1, 'should have 1 frame before clear');
+  t.equal(fs["_prevByte255"], true, "should have prevByte before clear");
+  t.equal(fs.frameCount, 1, "should have 1 frame before clear");
 
-    fs.clear();
-    t.equal(fs["_prevByte255"], false, 'should not have prevByte after clear');
-    t.equal(fs.frameCount, 0, 'should have 0 frames after clear');
-    
-    t.end();
+  fs.clear();
+  t.equal(fs["_prevByte255"], false, "should not have prevByte after clear");
+  t.equal(fs.frameCount, 0, "should have 0 frames after clear");
+
+  t.end();
 });
